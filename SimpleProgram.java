e8851d06c16c0ba9ca25e38deaac23a263dd6ad2
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2024-11-30 23:18:27 +0530

    Adv Java

---
+++ SimpleProgram.java
@@ -0,0 +1,14 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+
+        // Perform a basic arithmetic operation
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
